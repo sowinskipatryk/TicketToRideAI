707a4db2b364125333f8673f6227da75482dfae6
sowinskipatryk <91700001+sowinskipatryk@users.noreply.github.com>
2024-08-13 23:57:48 +0200

    data split for each version, some files missing

--- game_logic/utils.py
+++ game_logic/utils.py
@@ -5,38 +5,32 @@ from typing import List
 current_dir = os.path.dirname(__file__)
 
 
-def load_cities() -> List[str]:
-    file_path = os.path.join(current_dir, 'data', 'cities.txt')
-    with open(file_path, 'r') as file:
-        lines = file.readlines()
-
+def load_cities(version: str) -> List[str]:
+    file_path = os.path.join(current_dir, 'data', version, 'cities.txt')
     cities = []
-    for line in lines:
-        city = line.strip()
-        cities.append(city)
+    with open(file_path, 'r', encoding='utf-8') as file:
+        for line in file:
+            city = line.strip()
+            cities.append(city)
     return cities
 
 
-def load_routes() -> List[List[str]]:
-    file_path = os.path.join(current_dir, 'data', 'routes.txt')
-    with open(file_path, 'r') as file:
-        lines = file.readlines()
-
+def load_routes(version: str) -> List[List[str]]:
+    file_path = os.path.join(current_dir, 'data', version, 'routes.txt')
     routes = []
-    for line in lines:
-        sline = line.strip()
-        route = sline.split(', ')
-        routes.append(route)
+    with open(file_path, 'r', encoding='utf-8') as file:
+        for line in file:
+            sline = line.strip()
+            route = sline.split(', ')
+            routes.append(route)
     return routes
 
 
-def load_tickets() -> collections.deque:
-    file_path = os.path.join(current_dir, 'data', 'tickets.txt')
-    with open(file_path, 'r') as file:
-        lines = file.readlines()
-
+def load_tickets(version: str) -> collections.deque:
+    file_path = os.path.join(current_dir, 'data', version, 'tickets.txt')
     tickets = collections.deque()
-    for line in lines:
-        parts = line.strip().split(', ')
-        tickets.append((parts[0], parts[1], int(parts[2])))
+    with open(file_path, 'r', encoding='utf-8') as file:
+        for line in file:
+            parts = line.strip().split(', ')
+            tickets.append((parts[0], parts[1], int(parts[2])))
     return tickets
